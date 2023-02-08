ac727c7851de1abd4e90e279580e73b952ecf59d
Marcos Jesus <marcosjesus.dev@gmail.com>
2023-02-08 11:16:56 -0300

    upgoing API

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
